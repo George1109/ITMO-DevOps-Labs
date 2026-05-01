5d6c1d6a3fa4d4de63ce6a25809e7ce7e35c494b
George1109 <65457445+George1109@users.noreply.github.com>
2026-05-02 00:04:11 +0300

    Test

--- Lab-4/Bot/test_config.py
+++ Lab-4/Bot/test_config.py
@@ -1,5 +1,5 @@
 import os
-import pytest
+
 
 def test_environment_variables():
     # Проверяем, что адрес Vault прокинут в систему
@@ -7,6 +7,7 @@ def test_environment_variables():
     vault_addr = os.getenv("VAULT_ADDR")
     assert vault_addr is not None, "VAULT_ADDR is not set in environment"
     assert "vault" in vault_addr, f"Expected vault in address, got {vault_addr}"
+
 
 def test_bot_token_placeholder():
     # Проверяем, что у нас хотя бы есть логика загрузки токена
